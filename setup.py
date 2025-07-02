db60931a8a1eeeb1adb0ba925ec90e4053c56574
Shuyuan Yang <sxy841@case.edu>
2025-07-02 19:46:07 -0400

    fix warning

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/resource', glob('resource/*')),
+        ('share/' + package_name + '/resource', glob('resource/*.npy')),
         ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
